42e541d8ec6eeba3517f59506e937a9b89c39481
norielu <nrc41826.nifty.com>
2017-01-28 01:56:00 +0000

    DELETEメソッドが呼ばれた際の処理を実装

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const server = http.createServer((req, res) => {
 				console.info('[' + now + '] Data posted: ' + data);
 			});
 			break;
+		case 'DELETE':
+			res.write('DELETE ' + req.url);
+			break;
 		default:
 			break;
 	}
