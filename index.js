8a5d7bced399dcbcfc67767738c6166bc7fb7743
newwave8 <new.wave.hatch@gmail.com>
2018-05-18 22:04:30 +0000

    DELETEメソッドに対応した処理を追加

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const server = http.createServer((req, res) => {
 				console.info('[' + now + '] Data posted: ' + body);
 			});
 			break;
+		case 'DELETE':
+			res.write('DELETE ' + req.url);
 		default:
 			break;
 	}
@@ -30,7 +32,7 @@ const server = http.createServer((req, res) => {
 }).on('clientError', (e) => {
 	console.error('[' + new Date() + '] Client Error', e);
 });
-const port = 8000;
+const port = 3000;
 server.listen(port, () => {
 	console.info('[' + new Date() + '] Listening on ' + port);
 });
