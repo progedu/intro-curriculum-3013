d0f4e35b37c1808920538244e757935d6a064bde
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-09-21 10:56:34 +0000

    Switchフォールスルーを修正

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const server = http.createServer((req, res) => {
 				body = Buffer.concat(body).toString();
 				console.info('[' + now + '] Data posted: ' + body);
 			});
+			break;
 		case 'DELETE':
 			res.write(`DELETE ${req.url}`);
 			break;
