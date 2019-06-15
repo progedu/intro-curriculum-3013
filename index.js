f17b4726b80d79648dcd9510a936746234ce8a36
sh00049m <sh00049m@gmail.com>
2019-06-15 15:38:50 +0000

    Adding 'Delete Method' into req.method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
-
+	console.log('req.methodの中身は' + req.method);
 	switch (req.method) {
 		case 'GET':
 			res.write('GET ' + req.url);
@@ -20,6 +20,9 @@ const server = http.createServer((req, res) => {
 				body = Buffer.concat(body).toString();
 				console.info('[' + now + '] Data posted: ' + body);
 			});
+			break;
+		case 'DELETE':
+			res.write('DELETE ' + req.url);
 			break;
 		default:
 			break;
