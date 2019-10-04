f7a5a250556639c732ac9361bb2de78026afce13
Asazuke <quusan3@gmail.com>
2019-10-04 20:19:42 +0900

    DELETEメゾット追加・アクセスログをJSONに記憶する機能・/logパスで中身を見れるように実装

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const fileName = './log.json';
+let log = new Map();
+try {
+    const allData = fs.readFileSync(fileName, 'utf8');
+    log = new Map(JSON.parse(allData));
+  } catch (ignore) {
+    console.log('過去ログ無し');
+	}
+	
 const server = http.createServer((req, res) => {
-	const now = new Date();
-	console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+	var now = new Date();
+	let time_Obj = {
+	month:now.getMonth() + 1,
+	day:now.getDate(),
+	year:now.getFullYear(),
+	week:["日","月","火","水","木","金","土"][(now.getDay())],
+	Hours:now.getHours(),
+	Minutes:now.getMinutes(),
+	Seconds:now.getSeconds()
+	}
+	let logData = `${time_Obj.year}/${time_Obj.month}/${time_Obj.day}(${time_Obj.week}) ${time_Obj.Hours}:${time_Obj.Minutes}:${time_Obj.Seconds}「${req.connection.remoteAddress}」からアクセス有り`;
+	log.set(now, logData);
+  saveTasks();
+  console.info(logData);
+
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
-
+	switch (req.url){
+		case '/log':
+			res.write(logData);
+				break;
+		default:
+				break;
+	}
 	switch (req.method) {
 		case 'GET':
 			res.write('GET ' + req.url);
 			break;
 		case 'POST':
 			res.write('POST ' + req.url);
-			let body = [];
+			let body = "";
 			req.on('data', (chunk) => {
-				body.push(chunk);
+				body += chunk;
 			}).on('end', () => {
-				body = Buffer.concat(body).toString();
+				//body = Buffer.concat(body).toString();
 				console.info('[' + now + '] Data posted: ' + body);
 			});
+		case 'DELETE':
+			res.write('DELETE' + req.url);
 			break;
 		default:
 			break;
@@ -34,3 +65,7 @@ const port = 8000;
 server.listen(port, () => {
 	console.info('[' + new Date() + '] Listening on ' + port);
 });
+
+function saveTasks() {
+	fs.writeFileSync(fileName, JSON.stringify(Array.from(log)), 'utf8');
+}
