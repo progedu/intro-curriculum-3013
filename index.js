49bdde531f4ed3890c42444f235dde59c3efb9dc
Kenichi Yukihira <tyanyuki3.39@gmail.com>
2020-02-01 12:22:17 +0100

    DELETEメソッドの追加

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-
-  switch (req.method) {
-    case 'GET':
-      res.write('GET ' + req.url);
-      break;
-    case 'POST':
-      res.write('POST ' + req.url);
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        console.info('[' + now + '] Data posted: ' + rawData);
-      });
-      break;
-    default:
-      break;
-  }
-  res.end();
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+	// console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+	const now = new Date();
+	console.info(`[${now}] Requested by ${req.connection.remoteAddress}`);
+	res.writeHead(200, {
+		'Content-Type': 'text/plain; charset=utf-8'
+	});
+	// res.write(req.headers['user-agent']);
+	
+	switch (req.method) {
+		case 'GET':
+			res.write(`GET ${req.url}`);
+			break;
+		case 'POST':
+			res.write(`POST ${req.url}`);
+			let rawData = '';
+			req.on('data', chunk => {
+				rawData = rawData + chunk;
+			}).on('end', () => {
+				console.info(`[${now}] Data posted: ${rawData}`);
+			});
+			break;
+		case 'DELETE':
+			res.write(`DELETE ${req.url}`);
+			break;	
+		default:
+			break;
+	}
+	res.end();
+}).on('error', e => {
+	console.error('[' + new Date() + '] Server Error', e);
+}).on('clientError', e => {
+	console.error('[' + new Date() + '] Client Error', e);
 });
-const port = 8000;
-server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+
+const port = 8080;
+server.listen(port, 'localhost', () => {
+	console.info('[' + new Date() + '] Listening on ' + port);
 });
