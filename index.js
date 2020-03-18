9a07be404349dc02cee95ca349aca2d78f75c7fb
poppinkaz <kazuki-suzuki@keio.jp>
2020-03-18 18:19:45 +0900

    DELETE処理の追加

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-
+  
   switch (req.method) {
     case 'GET':
       res.write('GET ' + req.url);
       break;
     case 'POST':
       res.write('POST ' + req.url);
-      let rawData = '';
+      let rawData = ' ';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
+      break;
+    case 'DELETE':
+      res.write('DELETE ' + req.url);
       break;
     default:
       break;
