cc3d6c9bb2b591de90225879cd26edda5322ec8a
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-04-02 19:26:17 +0900

    post リクエストの主データを配列から文字列に変換

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const server = http.createServer((req, res) => {
 			break;
 		case 'POST':
 			res.write(`POST ${req.url}`);
-			let body = [];
+			let body = '';
 			req.on('data', (chunk) => {
-				body.push(chunk);
+				body += chunk;
 			}).on('end', () => {
-				body = Buffer.concat(body).toString();
 				console.info(`${now} Data posted: ${body}`);
 			});
 			break;
