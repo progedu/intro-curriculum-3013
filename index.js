af1c9866176dd914f8265c8d5d74995dfe821c47
sousvillage <sousvillage@gmail.com>
2020-04-02 14:41:18 +0900

    DELETEメソッドの実装

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ const server = http.createServer((req, res) => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
+    case 'DELETE':
+      res.write('DELETE ' + req.url);
+      break;
     default:
       break;
   }
   res.end();
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('[' + now + '] Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('[' + now + '] Client Error', e);
 });
 const port = 8000;
 server.listen(port, () => {
