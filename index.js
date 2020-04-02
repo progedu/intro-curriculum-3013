6ca5e84f8da2a4f06eb5e7c9cb070e6c838ee073
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-04-02 19:09:21 +0900

    文字列連結にシングルクォーテーションを使用

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
 	const now = new Date();
-	console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+	console.info(`${now} Requested by ${req.connection.remoteAddress}`);
 	res.writeHead(200, {
 		'Content-Type': 'text/plain; charset=utf-8'
 	});
 
 	switch (req.method) {
 		case 'GET':
-			res.write('GET ' + req.url);
+			res.write(`GET ${req.url}`);
 			break;
 		case 'POST':
-			res.write('POST ' + req.url);
+			res.write(`POST ${req.url}`);
 			let body = [];
 			req.on('data', (chunk) => {
 				body.push(chunk);
 			}).on('end', () => {
 				body = Buffer.concat(body).toString();
-				console.info('[' + now + '] Data posted: ' + body);
+				console.info(`${now} Data posted: ${body}`);
 			});
 			break;
 		default:
@@ -26,11 +26,11 @@ const server = http.createServer((req, res) => {
 	}
 	res.end();
 }).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
+	console.error(`[ ${now} ] Server Error`,e);
 }).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
+	console.error(`[ ${now} ] Client Error`, e);
 });
 const port = 8000;
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+	console.info(`[ ${new Date()} ] Listeing on ${port}`);
 });
