1a0b79c419198779f752cab955bbdae5d91ede5e
kgSuzuki1994 <takuti0511@gmail.com>
2020-04-11 22:03:18 +0900

    DELETEメソッドが呼ばれた場合の処理を実装

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const server = http.createServer((req, res) => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
+    case 'DALETE':
+      res.write('DALETE ' + req.url);
+      break;
     default:
       break;
   }
