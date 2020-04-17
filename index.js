cf73af3f2c6fdce931b9441c8de6802b835d7384
mamizu <lifespring.123452@gmail.com>
2020-04-17 18:21:29 +0000

    実装完了

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ const server = http.createServer((req, res) => {
 
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+      res.write('GET ' + req.url + '\n');
       break;
     case 'POST':
-      res.write('POST ' + req.url);
+      res.write('POST ' + req.url + '\n');
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
       }).on('end', () => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
+      break;
+    case 'DELETE':
+      res.write('DELETE ' + req.url + '\n');
       break;
     default:
       break;
