502fd1158862682ef7515bdc46cbe7e4d0a80bc2
Haggy Kusanagi <artkit44t@gmail.com>
2020-05-03 12:41:11 +0900

    DELETEメソッドを実装

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ const server = http.createServer((req, res) => {
       res.write('POST ' + req.url);
       let rawData = '';
       req.on('data', (chunk) => {
-        rawData = rawData + chunk;
+        rawData += chunk;
       }).on('end', () => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
+
+    case 'DELETE':
+       res.write('DELETE', + req.url);
+      break;
+
     default:
       break;
   }
