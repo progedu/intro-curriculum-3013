959b7d743e084e94c84a3a2d2c9badbf4e21b104
momiji51 <aualk00950@yahoo.co.jp>
2020-05-03 11:35:27 +0900

    完成しました

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const server = http.createServer((req, res) => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
+    cace 'DELETE':
+      res.write('DELETE' + req.url);
+      break;
     default:
       break;
   }
