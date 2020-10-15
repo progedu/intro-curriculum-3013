5e2c476367ecfa72128b07c7c65666477564cbc0
ck-n2020 <ckckck_009@yahoo.co.jp>
2020-10-15 23:37:55 +0900

    インデントの修正

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = http.createServer((req, res) => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
-      case 'DELETE':
+    case 'DELETE':
         res.write('DELETE ' + req.url);
         break;
     default:
