1018c58d06a43273734daec924cb46c8aa94fefe
HappyKumamoto <zen.word+hub@gmail.com>
2020-10-17 15:30:07 +0900

    DELETEメソッドの実装

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const server = http.createServer((req, res) => {
 
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+      res.write('GET ' + req.url + '\n');
       break;
     case 'POST':
-      res.write('POST ' + req.url);
+      res.write('POST ' + req.url + '\n');
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
@@ -20,7 +20,10 @@ const server = http.createServer((req, res) => {
         console.info('[' + now + '] Data posted: ' + rawData);
       });
       break;
-    default:
+      case 'DELETE':
+    res.write('DELETE ' + req.url + '\n');
+      break;
+      default:
       break;
   }
   res.end();
