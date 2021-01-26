68244c8aa3c1c918c7111be7bb9001037c45e0fa
aozametatuki <akomatsu1224@gmail.com>
2021-01-26 16:04:25 +0900

    DELETEメソッドを追記

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 'use strict';
-const http = require('http');
-const server = http.createServer((req, res) => {
-	const now = new Date();
-	console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-	res.writeHead(200, {
-		'Content-Type': 'text/plain; charset=utf-8'
-	});
 
-	switch (req.method) {
-		case 'GET':
-			res.write('GET ' + req.url);
+// httpのモジュールを読み込む
+const http = require('http');
+
+// サーバーを作成
+const server = http.createServer((req, res) => {
+    // ログを出力
+    const now = new Date();
+    console.info(`[ ${now} Requested by ${req.connection.remoteAddress}`);
+    // レスポンスヘッダ
+    res.writeHead(200, {
+        'Content-Type' : 'text/plain; charset=utf-8'
+    });
+    // レスポンスの内容
+    switch(req.method){
+        // GETメソッドの場合
+        case 'GET':
+            res.write('GET ' + req.url);
+            break;
+        // POSTメソッドの場合
+        case 'POST':
+            res.write('POST ' + req.url);
+            let rawData = '';
+            req.on('data', (chunk) => {
+                rawData = rawData + chunk;
+            }).on('end', () => {
+                console.info(`[${now}] Data posted: ` + rawData);
+            });
 			break;
-		case 'POST':
-			res.write('POST ' + req.url);
-			let body = [];
-			req.on('data', (chunk) => {
-				body.push(chunk);
-			}).on('end', () => {
-				body = Buffer.concat(body).toString();
-				console.info('[' + now + '] Data posted: ' + body);
-			});
-			break;
-		default:
-			break;
-	}
-	res.end();
+		// DELETEメソッドの場合
+		case 'DELETE':
+			res.write('DELETE ' + req.url);
+        default:
+            break;
+    }
+    res.end();
+// サーバエラーのエラーログを出力
 }).on('error', (e) => {
-	console.error('[' + new Date() + '] Server Error', e);
+    console.error(`[ ${new Date()} ] Server Error`, e);
+// クライアントエラーのエラーログを出力
 }).on('clientError', (e) => {
-	console.error('[' + new Date() + '] Client Error', e);
+    console.error(`[ ${new Date()} ] client Error`, e);
 });
+
+// httpが起動するポートを設定
 const port = 8000;
+// サーバーを起動
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+	// ログを出力
+	console.info(`[ ${new Date()} ] Listening on ${port}`);
 });
