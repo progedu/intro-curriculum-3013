1a0dd277cbc5ed8dce87e2e236a522931c74e4d4
Manul Neko <spica_arcturus@msn.com>
2021-02-20 01:34:52 +0900

    DELETEメソッドの処理を実装

--- index.js
+++ index.js
@@ -8,20 +8,23 @@ const server = http.createServer((req, res) => {
   });
 
   switch (req.method) {
-    case 'GET':
-      res.write('GET ' + req.url);
-      break;
-    case 'POST':
-      res.write('POST ' + req.url);
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        console.info('[' + now + '] Data posted: ' + rawData);
-      });
-      break;
-    default:
-      break;
+	  case 'GET':
+		  res.write('GET ' + req.url);
+		  break;
+	  case 'POST':
+		  res.write('POST ' + req.url);
+		  let rawData = '';
+		  req.on('data', (chunk) => {
+			  rawData = rawData + chunk;
+		  }).on('end', () => {
+			  console.info('[' + now + '] Data posted: ' + rawData);
+		  });
+		  break;
+	  case 'DELETE':
+		  res.write('DELETE ' + req.url);
+		  break;
+	  default:
+		  break;
   }
   res.end();
 }).on('error', (e) => {
